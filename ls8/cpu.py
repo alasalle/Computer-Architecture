af87f4ee963f42913f7dc9bee81fc6241eb7bc27
alasalle <angela.lasalle91@gmail.com>
2019-08-24 00:48:42 -0400

    system stack implemented

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,6 +6,8 @@ LDI = 0b10000010
 PRN = 0b01000111
 HLT = 0b00000001
 MUL = 0b10100010
+POP = 0b01000110
+PUSH = 0b01000101
 
 class CPU:
     """Main CPU class."""
@@ -14,11 +16,17 @@ class CPU:
         """Construct a new CPU."""
         self.ram = [0] * 256
         self.reg = [0] * 8
+        self.sp = 0xF4
         self.dispatch_table = {}
         self.dispatch_table[LDI] = self.handle_LDI
         self.dispatch_table[PRN] = self.handle_PRN
         self.dispatch_table[MUL] = self.handle_MUL
         self.dispatch_table[HLT] = self.handle_HLT
+        self.dispatch_table[POP] = self.handle_POP
+        self.dispatch_table[PUSH] = self.handle_PUSH
+        self.alu_dispatch_table = {}
+        self.alu_dispatch_table['ADD'] = self.alu_ADD
+        self.alu_dispatch_table['MUL'] = self.alu_MUL
         self.running = True
         self.pc = 0
 
@@ -42,6 +50,30 @@ class CPU:
     def handle_HLT(self):
         self.running = False
 
+    def handle_POP(self):
+        operand_a = self.ram_read(self.pc + 1)
+
+        if self.sp == 0xF4:
+            print("Stack is empty")
+            sys.exit(1)
+        else:
+            self.sp += 1
+            self.reg[operand_a] = self.ram[self.sp]
+            self.pc += 2
+
+    def handle_PUSH(self):
+        operand_a = self.ram_read(self.pc + 1)
+
+        self.ram[self.sp] = self.reg[operand_a]
+        self.sp -= 1
+        self.pc += 2
+
+    def alu_ADD(self, reg_a, reg_b):
+        self.reg[reg_a] = (self.reg[reg_a] + self.reg[reg_b]) & 0xFF
+
+    def alu_MUL(self, reg_a, reg_b):
+        self.reg[reg_a] = (self.reg[reg_a] * self.reg[reg_b]) & 0xFF
+
 
     def ram_read(self, address):
         return self.ram[address]
@@ -56,26 +88,35 @@ class CPU:
 
         program = []
 
-        with open(sys.argv[1]) as b:
-            for line in b:
-                if line.startswith('0') | line.startswith('1'):
-                    number = line.split('#')[0].strip()
-                    line = int(number, 2)
-                    program.append(line)
+        try:
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+            with open(sys.argv[1]) as b:
+                for line in b:
+                    if line.startswith('0') | line.startswith('1'):
+                        l = line.split('#')[0].strip()
+                        number = int(l, 2)
+                        program.append(number)
+
+            for instruction in program:
+                self.ram[address] = instruction
+                address += 1
+
+        except:
+            print("Oops!",sys.exc_info()[0],"occured in load function.")
+            sys.exit(1)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
-        if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        elif op == "MUL":
-            self.reg[reg_a] *= self.reg[reg_b]
-        else:
-            raise Exception("Unsupported ALU operation")
+        try:
+
+            self.alu_dispatch_table[op](reg_a, reg_b)
+
+        except:
+
+            print("Oops!",sys.exc_info()[0],"occured in alu function.")
+            sys.exit(1)
+
 
     def trace(self):
         """
@@ -100,17 +141,15 @@ class CPU:
     def run(self):
         """Run the CPU."""
 
-        # * `LDI`: load "immediate", store a value in a register, or "set this register to
-        # this value". 10000010
-        # * `PRN`: a pseudo-instruction that prints the numeric value stored in a
-        # register. 01000111
-        # * `HLT`: halt the CPU and exit the emulator. 00000001
-
-
-        
-
         while self.running:
 
-            IR = self.ram[self.pc]
+            try:
 
-            self.dispatch_table[IR]()
+                IR = self.ram[self.pc]
+
+                self.dispatch_table[IR]()
+            
+            except:
+
+                print("Oops!",sys.exc_info()[0],"occured in run function.")
+                sys.exit(1)
